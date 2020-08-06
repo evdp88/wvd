a163948551036e7e509ff2fe585e685d6422c47e
evdp88 <68423414+evdp88@users.noreply.github.com>
2020-08-06 10:39:19 +0200

    Create OptimizeWVD.ps1

---
+++ PowerShell/OptimizeWVD.ps1
@@ -0,0 +1,46 @@
+<#Author       : Dean Cefola
+# Creation Date: 07-09-2020
+# Usage        : Windows Virtual Desktop Optimization Script
+# All code that this script executes is created and provided by THE VDI GUYS
+# You can download the code here  --  https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool
+#********************************************************************************
+# Date                         Version      Changes
+#------------------------------------------------------------------------
+# 07/09/2020                     1.0        Intial Version
+#
+
+#*********************************************************************************
+#
+#>
+
+
+################################
+#    Download WVD Optimizer    #
+################################
+New-Item -Path C:\ -Name Optimize -ItemType Directory -ErrorAction SilentlyContinue
+$LocalPath = "C:\Optimize\"
+$WVDOptimizeURL = 'https://github.com/evdp88/wvd/WVD_optimization/archive/master.zip'
+$WVDOptimizeInstaller = "WVD_optimization.zip"
+Invoke-WebRequest `
+    -Uri $WVDOptimizeURL `
+    -OutFile "$Localpath$WVDOptimizeInstaller"
+
+
+###############################
+#    Prep for WVD Optimize    #
+###############################
+Expand-Archive `
+    -LiteralPath "C:\Optimize\WVD_optimization.zip" `
+    -DestinationPath "$Localpath" `
+    -Force `
+    -Verbose
+Set-Location -Path C:\Optimize\Virtual-Desktop-Optimization-Tool-master
+
+
+#################################
+#    Run WVD Optimize Script    #
+#################################
+New-Item -Path C:\Optimize\ -Name install.log -ItemType File -Force
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force -Verbose
+add-content c:\Optimize\install.log "Starting Optimizations"  
+.\Win10_VirtualDesktop_Optimize.ps1 -WindowsVersion 2004 -Restart -Verbose
