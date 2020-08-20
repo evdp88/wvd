a82cb167d99870f85e2d3c06a4d719cb3ba496d7
evdp88 <68423414+evdp88@users.noreply.github.com>
2020-08-20 10:33:01 +0200

    Update New-WVDSessionHost.ps1

--- PowerShell/New-WVDSessionHost.ps1
+++ PowerShell/New-WVDSessionHost.ps1
@@ -56,14 +56,6 @@ $Win7x64_WVDBootMgrURI   = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/b
 ####################################
 #    Test/Create Temp Directory    #
 ####################################
-New-Item -Path c:\ -Name New-WVDSessionHost.log -ItemType File
-Add-Content `
--LiteralPath C:\New-WVDSessionHost.log `
-"
-ProfilePath       = $ProfilePath
-RegistrationToken = $RegistrationToken
-Optimize          = $Optimize
-"
 
 if((Test-Path c:\temp) -eq $false) {
     Add-Content -LiteralPath C:\New-WVDSessionHost.log "Create C:\temp Directory"
@@ -95,7 +87,14 @@ else {
         -BackgroundColor Black `
         "c:\temp\wvd directory already exists"
 }
-
+New-Item -Path c:\ -Name New-WVDSessionHost.log -ItemType File
+Add-Content `
+-LiteralPath C:\New-WVDSessionHost.log `
+"
+ProfilePath       = $ProfilePath
+RegistrationToken = $RegistrationToken
+Optimize          = $Optimize
+"
 
 #################################
 #    Download WVD Componants    #
